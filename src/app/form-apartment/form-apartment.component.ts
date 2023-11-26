3d442cc7dea8227730b90c2b3048413a1c832289
Baha Rojbi <101024667+Baha-Rojbi@users.noreply.github.com>
2023-11-26 13:17:05 +0100

    Created the form

--- src/app/form-apartment/form-apartment.component.ts
+++ src/app/form-apartment/form-apartment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-apartment',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-apartment.component.css']
 })
 export class FormApartmentComponent {
+  apartmentForm=new FormGroup({
+    apartmentNumber: new FormControl('',[Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    flourNumber: new FormControl('',[Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    surface: new FormControl('',[Validators.required]),
+    terrace: new FormControl(''),
+    surfaceTerrace: new FormControl({ value: '', disabled: true }, Validators.required),
+    category: new FormControl([Validators.required]),
+    description: new FormControl('',[Validators.required, Validators.minLength(10)]),
+    residence: new FormControl('',[Validators.required]),
+  }); 
+   // Activer ou désactiver la Surface Terrace en fonction de la valeur de Terrace
+   onTerraceChange() {
+    const terraceControl = this.apartmentForm.get('terrace');
+    const terraceSurfaceControl = this.apartmentForm.get('terraceSurface');
 
-}
+    if (terraceControl?.value === 'yes') {
+      terraceSurfaceControl?.enable();
+    } else {
+      terraceSurfaceControl?.disable();
+    }
+  }
+  }
